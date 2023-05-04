349b2682a70f92e7cfc43bfd23e64025ab21f9d9
Hettore Eduardo <duh.barretoo@gmail.com>
2023-05-03 21:24:21 -0300

    Aula final do módulo back end - prof André 03/05/23

---
+++ api.py
@@ -0,0 +1,56 @@
+from flask import Flask, jsonify, request
+import repositorio
+
+app = Flask(__name__)
+
+
+@app.route("/products", methods=['GET'])
+def get_produtos():
+    produtos = repositorio.retornar_produtos()
+    print(produtos)
+    print(type(produtos))
+    return jsonify(produtos), 200
+
+@app.route("/product/<int:id>", methods=['GET'])
+def get_produto(id):
+    produto = repositorio.retornar_produto(id)
+    if produto:
+        return jsonify(produto)
+    else:
+        return jsonify({"message": "Produto não encontrado"}), 404
+
+@app.route("/product", methods=['POST'])
+def set_produto():
+    produto = request.json
+    id = repositorio.criar_produto(**produto)
+    if id:
+        produto["id"] = id
+        return jsonify(produto), 201
+    else:
+        return jsonify({"message": "não foi possível criar o produto"}), 500
+
+@app.route("/product/<int:id>", methods=['PUT'])
+def update_produto(id):
+    produto = repositorio.retornar_produto(id)
+    if produto:
+        dados_atualizados = request.json
+        dados_atualizados["id"] = id
+        if repositorio.atualizar_produto(**dados_atualizados):
+            return jsonify(dados_atualizados), 200
+        else:
+            return jsonify({"message": "não foi possível atualizar"}), 500
+    else:
+        return jsonify({"message": "produto não localizado"}), 404
+    
+@app.route("/product/<int:id>", methods=['DELETE'])
+def delete_produto(id):
+    produto = repositorio.retornar_produto(id)
+    if produto:
+        if repositorio.remover_produto(id):
+            return jsonify({"message": "produto excluído"}), 200
+        else:
+            return jsonify({"message": "não foi possível remover o produto"}), 500
+    else:
+        return jsonify({"message": "produto não localizado"}), 404
+
+app.run(debug=True)
---
+++ repositorio.py
@@ -0,0 +1,98 @@
+import sqlite3
+import os
+
+caminho = f"{os.path.dirname(__file__)}\\db\\produtos.db"
+
+#id generator
+def gerar_id():
+    conn = sqlite3.connect(caminho)
+    cursor = conn.cursor()
+    cursor.execute("SELECT seq FROM sqlite_sequence WHERE name='produtos'")
+    id = cursor.fetchone()[0]
+    conn.close()
+    return id + 1
+
+#create
+def criar_produto(nome:str, descricao:str, preco:float, imagem:str):
+    try:
+        conn = sqlite3.connect(caminho)
+        cursor = conn.cursor()
+        sql_insert = "INSERT INTO produtos (nome_produto, descricao_produto, preco_produto, imagem_produto) VALUES (?, ?, ?, ?)"
+        cursor.execute(sql_insert, (nome, descricao, preco, imagem))
+        id = cursor.lastrowid
+        conn.commit()
+        conn.close()
+        return id
+    except Exception as ex:
+        print(ex)
+        return 0
+
+#update
+def atualizar_produto(id:int, nome:str, descricao:str, preco:float, imagem:str):
+    try:
+        conn = sqlite3.connect(caminho)
+        cursor = conn.cursor()
+        sql_update = "UPDATE produtos SET nome_produto = ?, descricao_produto = ?, preco_produto = ?, imagem_produto = ? WHERE id_produto = ?"
+        cursor.execute(sql_update, (nome, descricao, preco, imagem, id))
+        conn.commit()
+        conn.close()
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
+    
+#delete
+def remover_produto(id:int):
+    try:
+        conn = sqlite3.connect(caminho)
+        cursor = conn.cursor()
+        sql_delete = "DELETE FROM produtos WHERE id_produto = ?"
+        cursor.execute(sql_delete, (id, ))
+        conn.commit()
+        conn.close()
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
+
+#read
+def retornar_produto(id:int) -> tuple: 
+    try:
+        if id == 0:
+            return gerar_id(), "", "", "", ""
+        conn = sqlite3.connect(caminho)
+        cursor = conn.cursor()
+        sql_select = "SELECT * FROM produtos WHERE id_produto = ?"
+        cursor.execute(sql_select, (id, ))
+        id, nome, descricao, preco, imagem = cursor.fetchone()
+        
+        conn.close()
+        return {'id':id, 'nome':nome, 'descricao':descricao, 'preco':preco, 'imagem':imagem}
+    except Exception as ex:
+        print(ex)
+        return False
+
+def retornar_produtos() -> list:
+    try:
+        conn = sqlite3.connect(caminho)
+        cursor = conn.cursor()
+        sql_select = "SELECT * FROM produtos"
+        cursor.execute(sql_select)
+        dados = cursor.fetchall()
+        conn.close()
+        produtos = []
+        for item in dados:
+            produto = {}
+            produto['id'] = item[0]
+            produto['nome'] = item[1]
+            produto['descricao'] = item[2]
+            produto['preco'] = item[3]
+            produto['imagem'] = item[4]
+            produtos.append(produto)
+        return produtos
+    except Exception as ex:
+        print(ex)
+        return False
+
+
+
